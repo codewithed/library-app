707f948edf4f686e04aa9a26cc5356f625411dda
Edmund Debrah <edmundking05@gmail.com>
2022-12-22 15:41:01 +0000

    add book constructor

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+  this.info = () => `The ${title} by ${author} has ${pages} ${readStatus}`;
+}
