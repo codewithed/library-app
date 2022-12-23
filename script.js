57f49231cedbb61c2223dc6e82e991f53ad281f3
Edmund Debrah <edmundking05@gmail.com>
2022-12-23 15:27:08 +0000

    add displayBooks function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const librarySection = document.querySelector('.library-section');
+
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
@@ -10,6 +12,19 @@ function Book(title, author, pages, readStatus) {
 
 function addBooktoLibrary() {
   // take user's input and store new book into an array
-  const book = new Book('$title', '$author', '$pages', '$readStatus');
+  const book = new Book(title, author, pages, readStatus);
   myLibrary.push(book);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+    bookCard.innerHTML = `<p>${book.tile}</p>
+                      <p>${book.author}</p>
+                      <p>${book.pages} pages</p>
+                      <button>${book.readStatus}</button>
+                      <button>Remove</button>`;
+    librarySection.appendChild(bookCard);
+  });
 }
