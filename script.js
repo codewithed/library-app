637aa3c5e7a744be5448662ac79374357f282c2d
Edmund Debrah <edmundking05@gmail.com>
2022-12-23 14:44:23 +0000

    add addBooktoLibrary function

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function Book(title, author, pages, readStatus) {
   this.readStatus = readStatus;
   this.info = () => `The ${title} by ${author} has ${pages} ${readStatus}`;
 }
+
+function addBooktoLibrary() {
+  // take user's input and store new book into an array
+  const book = new Book('$title', '$author', '$pages', '$readStatus');
+  myLibrary.push(book);
+}
