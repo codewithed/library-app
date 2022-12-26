85f516873830bb210e60e3dd4a403e904a295abc
Edmund Debrah <edmundking05@gmail.com>
2022-12-26 21:59:47 +0000

    add remove button functionality

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ const form = document.createElement('form');
 form.classList.add('book-form');
 form.innerHTML = `<form class="form" id="form" method="post" action>
                         <legend>Add New Book</legend>
-                        <input type="text" data-id="title" name="title" placeholder="title" required>
-                        <input type="text" data-id="author" name="author" placeholder="author" required>
-                        <input type="text" data-id="pages" name="pages" placeholder="pages" required>
-                        <div>Have you read it?<input data-id="readStatus" type="checkbox"></div>
+                        <input type="text" name="title" placeholder="title" required>
+                        <input type="text" name="author" placeholder="author" required>
+                        <input type="text" name="pages" placeholder="pages" required>
+                        <label>Have you read it?</label><input id="checkbox" type="checkbox">
                         <button id='submit' type='submit'>Add</button>
                     </form>`;
 
@@ -29,18 +29,39 @@ function addBooktoLibrary() {
   const title = formData.get('title');
   const author = formData.get('author');
   const pages = formData.get('pages');
-  const newbook = new Book(title, author, pages);
+  const checkBox = document.getElementById('checkbox');
+
+  let readStatus;
+  if (checkBox.checked === true) {
+    readStatus = 'read';
+  } else if (checkBox.checked === false) {
+    readStatus = 'unread';
+  }
+  const newbook = new Book(title, author, pages, readStatus);
 
   // add newBook to display
   const bookCard = document.createElement('div');
-  bookCard.classList.add('book-card');
-  bookCard.innerHTML = `<p>${newbook.title}</p>
+
+  bookCard.classList.add('bookCard');
+  bookCard.innerHTML = `<p>"${newbook.title}"</p>
                       <p>${newbook.author}</p>
                       <p>${newbook.pages} pages</p>
-                      <button>${newbook.readStatus}</button>
-                      <button>Remove</button>`;
+                      <button id="readStatusBtn">${newbook.readStatus}</button>
+                      <button data-link id="remove">Remove</button>`;
   librarySection.appendChild(bookCard);
   myLibrary.push(newbook);
+
+  const removeBtns = document.querySelectorAll('#remove');
+  const bookCards = document.getElementsByClassName('bookCard');
+  for (let i = 0; i < removeBtns.length; i += 1) {
+    removeBtns[i].dataset.link = i;
+
+    // remove the book
+    removeBtns[i].addEventListener('click', () => {
+      myLibrary.splice(i, 1);
+      librarySection.removeChild(bookCards[i]);
+    });
+  }
 }
 
 // displays the form on the screen
@@ -50,7 +71,6 @@ addBtn.addEventListener('click', () => {
 });
 
 // creates new book from form data
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   addBooktoLibrary();
